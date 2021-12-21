af9485cc3cee025244e846514bb3ebd10f4ee31c
Nayeem <nayeem.ahmedemba@gmail.com>
2021-12-22 00:06:44 +0600

    spring boot-angular-font end1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { LoginComponent } from './components2/login/login.component';
+import { SignupComponent } from './components2/signup/signup.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: "", component: LayoutComponent, children: [
+    {path: '', component: HomeComponent},
+  ]
+  },
+  {path:"login", component:LoginComponent}, 
+  {path:"signup", component:SignupComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+import { LayoutComponent } from './components/layout/layout.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { LoginComponent } from './components2/login/login.component';
+import { SignupComponent } from './components2/signup/signup.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LayoutComponent,
+    HomeComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
